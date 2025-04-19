b4162b2f3e9d264f66d9b28b6ade973652a164fb
DESKTOP-ETPCG4G\TWGUO <tibameernie@gmail.com>
2025-04-19 13:44:27 +0800

    初始化專案Git版控檔案設定

---
+++ src/main/java/tw/idv/nameless/nameless/NamelessApplication.java
@@ -0,0 +1,13 @@
+package tw.idv.nameless.nameless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamelessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamelessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/idv/nameless/nameless/NamelessApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.idv.nameless.nameless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamelessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
